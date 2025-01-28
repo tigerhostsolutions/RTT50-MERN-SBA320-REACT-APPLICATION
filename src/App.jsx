e6a72c6fdb2d37c6650f3b46abf0aff254c8351b
tigerhostsolutions <jgrasty@tigerhost.domains>
2025-01-27 22:39:56 -0500

    created/updated: update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import { useState } from 'react'
-
+import React from 'react'
+import UserForm from './components/UserForm.jsx'
 import './App.css'
 
 function App() {
 
   return (
       <div>
-        <h1>Hello World</h1>
+        <h1>User Profile</h1>
+        <UserForm />
       </div>
 
   )
